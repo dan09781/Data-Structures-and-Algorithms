1ed2fe8bcf2b9dc06a55fcedd40d78109cbda31c
Daniel Min <daniel.min96@hotmail.com>
2021-03-05 19:46:50 -0500

    Completed insert function for hash table with open addressing
    - Time to test the insert function

--- HashTable/HashTableOpenAddressing.java
+++ HashTable/HashTableOpenAddressing.java
@@ -1,8 +1,9 @@
 //Hash Table implementation using open addressing for collision resolution
+//Use probing functions that ALWAYS result in a full cycle modulo
 
 import java.util.*;
 
-public class HashTable<T,V>{
+public class HashTableOpenAddressing<T,V>{
 
 	//First declare the entry class
 	private class Entry<T,V>{
@@ -41,17 +42,19 @@ public class HashTable<T,V>{
 	//to avoid index out of bound exception later on when accessing elements
 	//Whereas size represents the number of non-null elements in the array list
 	private int size=0;
-	private List<List<Entry<T,V>>> table;
+	private List<Entry<T,V>> table;
+	private final Entry<T,V> TOMBSTONE = (Entry<T,V>)(new Object());
+	private static final int LINEAR_CONSTANT = 1;
 
-	HashTable(){
+	HashTableOpenAddressing(){
 		this(defaultCapacity, defaultLoadFactor);
 	}
 
-	HashTable(int capacity){
+	HashTableOpenAddressing(int capacity){
 		this(capacity, defaultLoadFactor);
 	}
 
-	HashTable(int capacity, double loadFactor){
+	HashTableOpenAddressing(int capacity, double loadFactor){
 		//Capacity has to be greater or equal to 0 and load factor should be in the range (0,1]
 		if (capacity<0 || (loadFactor<=0 || loadFactor>1))
 			throw new IllegalArgumentException();
@@ -59,7 +62,7 @@ public class HashTable<T,V>{
 		this.capacity = capacity;
 		threshold = (int)(maxLoadFactor*capacity);
 		//List instead of LinkedList because we are not creating linked lists yet
-		table = new ArrayList<List<Entry<T,V>>>(capacity);
+		table = new ArrayList<Entry<T,V>>(capacity);
 		//Fill the table with nulls to avoid index out of bound error
 		clear();
 	}
@@ -79,7 +82,7 @@ public class HashTable<T,V>{
 		return (hashCode & 0x7FFFFFFF) % capacity;
 	}
 
-	//Clear hash table
+	//Clear hash table; set everything to null and reset size to 0
 	public void clear(){
 		for (int i=0;i<capacity;i++){
 			table.add(null);
@@ -87,155 +90,80 @@ public class HashTable<T,V>{
 		size = 0;
 	}
 
-	public boolean containsKey(T key){
-		if (isEmpty())
-			return false;
-		int tableIndex = getIndexFromHashCode(key.hashCode());
-		return listContainsKey(tableIndex,key);
+	//Function to find greatest common denomenator of two integers
+	protected int gcd(int a, int b){
+		if (b==0) return a;
+		return gcd(b,a%b);
 	}
 
-	//Function that finds if a key exists in a list
-	private boolean listContainsKey(int i, T key){
-		//If given index in the table does not exist
-		//Do not accept null keys
-		if (table.get(i)==null || key==null)
-			return false;
-		for (Entry<T,V> ent:table.get(i)){
-			if (key.equals(ent.key)){
-				return true;
-			}
-		}
-		return false;
+	//function to swap two elements on the table
+	private void swap(int i, int j){
+		Entry<T,V> temp = table.get(i);
+		Entry<T,V> temp2 = table.get(j);
+		table.set(i,temp2);
+		table.set(j,temp);
 	}
 
-	//Add new entry to the hash table
-	public void put(T key, V val){
+	//Some protected abstract methods to support quadratic and linear probing
+	protected abstract int probingFunction(int x);
+
+	protected abstract void adjustCapacity();
+
+	public void insert(T key, V val){
 		if (key==null)
 			throw new IllegalArgumentException("Key cannot be null");
-		Entry<T,V> newEntry = new Entry<>(key,val);
-		listInsertEntry(newEntry);
+
+		int tableIndex = getIndexFromHashCode(key.hashCode());
+
+		//if size exceeds threshold after insertion, resize table
+		//It's important to leave at least 1 slot null even if max load factor is 1
+		//to avoid infinite loop when probing
+		if (size+1>=threshold)
+			resizeTable();
+
+		//Initialize index of tombstone to be -1
+		int tombstoneExists = -1; 
+
+		//Initialize x to be 0. Will be used for probing function
+		int x = 0;
+
+
+		//loop until no collision is detected or a tombstone is found
+		while (table.get(tableIndex)!=null){
+			//If the first tombstone is found, save the index of the tombstone
+			//We are only interested in the first tombstone because that is 
+			//where we want to locate an entry
+			if (table.get(tableIndex)==TOMBSTONE && tombstoneExists==-1)
+				tombstoneExists=tableIndex;
+			//There is a collision with another entry, perhaps an entry with the same key already exists.
+			//In that case, we update the value and swap with the tombstone
+			else{
+				//If there already exists an entry with the same key
+				if (table.get(tableIndex).key.equals(key)){
+					//Update the value
+					table.get(tableIndex).val=val;
+					//swap the entry with the tombstone if a tombstone had been found
+					if (tombstoneExists!=-1){
+						swap(tableIndex,tombstoneExists);
+						//Set the old location of the entry to null
+						table.set(tableIndex,null);
+						size--;
+					}
+					//Job done. Return out of function
+					return;
+
+				}
+			}
+			//Update table index to keep probing
+			tableIndex+=probingFunction(x++);
+			tableIndex%=capacity;
+		}
+
+		//If we get to here, it means that an empty slot was found.
+		//We can just insert the key,val pair to this slot and increment the size
+		Entry<T,V> newEntry = new Entry<>(key, val);
+		table.set(tableIndex, newEntry);
+		size++;
 		return;
 	}
-
-	//Given an entry, insert it onto the appropriate list
-	private void listInsertEntry(Entry<T,V> ent){
-		//Get the table index by normalizing hash code
-		int tableIndex = getIndexFromHashCode(ent.key.hashCode());
-		//If this is the first entry being inserted into the table index, create new linked list
-		//and add the entry to the new list
-		if (table.get(tableIndex)==null){
-			//If the size of table is going to exceed threshold, need to resize
-			if (size+1 > threshold)
-				resizeTable();
-			List<Entry<T,V>> temp = new LinkedList<>();
-			//Since we are using arrayList, instead of doing table.get(tableIndex) = temp, which
-			//results in an error, use set() method
-			table.set(tableIndex,temp);
-			size++;
-			table.get(tableIndex).add(ent);
-		}
-		else{
-			//if entry already exists in the list, we update the value
-			if (listContainsKey(tableIndex,ent.key)){
-				for (Entry<T,V> tempEntry:table.get(tableIndex)){
-					if (tempEntry.key.equals(ent.key))
-						tempEntry.val = ent.val;
-				}
-			}
-			//if not, we just add it to the list
-			else
-				table.get(tableIndex).add(ent);
-		}
-
-		return;
-	}
-
-	//Given a key, return its corresponding value
-	public V get(T key){
-		int tableIndex = getIndexFromHashCode(key.hashCode());
-		//If the given key does not exist in table
-		if (!listContainsKey(tableIndex,key))
-			return null;
-		for (Entry<T,V> ent:table.get(tableIndex)){
-			if (key.equals(ent.key)){
-				return ent.val;
-			}
-		}
-		return null;
-	}
-
-	//Given a key, remove the entry corresponding to that key
-	public Entry<T,V> remove(T key){
-		int tableIndex = getIndexFromHashCode(key.hashCode());
-		//Key does not exist in table. Nothing to return
-		if (!listContainsKey(tableIndex,key))
-			return null;
-		//Key exists. Need to return the entry to be removed
-		for (Entry<T,V> ent:table.get(tableIndex)){
-			if (key.equals(ent.key)){
-				Entry<T,V> res = ent;
-				table.get(tableIndex).remove(ent);
-				//If no entry exists in the list after deletion, 
-				//delete the list and decrease the size
-				if (table.get(tableIndex).size()==0){
-					table.set(tableIndex,null);
-					size--;
-				}
-				return res;
-			}
-		}
-		return null;
-	}
-
-	//Resize table if number of elements in the table exceeds threshold
-	private void resizeTable(){
-		capacity*=2;
-		threshold=(int)(capacity*maxLoadFactor);
-		List<List<Entry<T,V>>> newTable = new ArrayList<List<Entry<T,V>>>(capacity);
-		//Need to copy all entries from old table to new table
-		for (int i=0;i<table.size();i++){
-			//if a list exists do the copying
-			if (table.get(i) != null){
-				//Copy every entry
-				for (Entry<T,V> ent:table.get(i)){
-					int tableIndex = getIndexFromHashCode(ent.hash);
-					//If no entries exist in index
-					//create new linked list and add the entry to the new linked list
-					if (newTable.get(tableIndex) == null){
-						newTable.set(tableIndex, new LinkedList<Entry<T,V>>());
-						newTable.get(tableIndex).add(ent);
-					}
-					//If there already is a linked list
-					//Just add the entry to the existing linked list
-					else{
-						newTable.get(tableIndex).add(ent);
-					}
-				}
-				//clear the list
-				table.get(i).clear();
-				table.set(i,null);
-			}
-		}
-		//Assign newTable as table
-		table=newTable;
-	}
-
-
-	@Override
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("{");
-		for (int i=0;i<capacity;i++){
-			if (table.get(i)==null)
-				continue;
-			for (Entry<T,V> ent:table.get(i)){
-				//We can just use ent since linked list already has its own toString 
-				//method that will convert ent into useful output
-				sb.append(i + ":" + ent + ", ");
-			}
-		}
-		sb.append("}");
-		return sb.toString();
-	}
-
 }
